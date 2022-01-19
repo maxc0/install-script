6762252aa4ee1d4c0e069d5ec79c69177057c0fd
Max <max@mail.net>
2022-01-19 16:31:29 +1100

    Create install-script.bat

---
+++ install-script.bat
@@ -0,0 +1,23 @@
+winget install -e --id Python.Python.3
+winget install -e --id VideoLAN.VLC
+winget install -e --id NordVPN.NordVPN
+winget install -e --id LibreWolf.LibreWolf
+winget install -e --id Oracle.VirtualBox
+winget install -e --id Anki.Anki
+winget install -e --id SublimeHQ.SublimeText.4
+winget install -e --id Lexikos.AutoHotkey
+winget install -e --id 9WZDNCRDJXP4 & :: PDF Reader by Xodo
+winget install -e --id IDRIX.VeraCrypt
+winget install -e --id Notion.Notion
+winget install -e --id Viber.Viber
+winget install -e --id Zoom.Zoom
+winget install -e --id Discord.Discord
+winget install -e --id Git.Git
+winget install -e --id Microsoft.VisualStudioCode
+winget install -e --id TeamViewer.TeamViewer
+winget install -e --id qBittorrent.qBittorrent
+
+:: MISSING
+:: Microsoft Office
+:: Adobe Photoshop CC 2015
+:: GG Poker
